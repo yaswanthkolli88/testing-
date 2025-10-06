7067b82c950a449be0facd22cc2d0e0ad733249d
yaswanthkolli88 <yaswanthkolli88@gmail.com>
2025-10-06 20:45:42 +0530

    added a new one

---
+++ auto.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#
+#
+#
+#
+#############################################
+#name : yaswanth kolli
+#date : 06/10/2025
+#
+#for scripting
+#
+#version : v1
+#
+#############################################
+#
+
+
+# created a file 
+mkdir scripting
+
+# entred into the created folder 
+cd scripting 
+
+mkdir basics   loops   file_handling   system   projects    # created this file in scripting folder  
+
+cd basics 
+touch hello.sh variable.sh arithmetic.sh conditions.sh
+cd ..
+
+
+
+cd loops
+touch for_loop.sh while_loop.sh even_numbers.sh multiplication_table.sh
+cd ..
+
+
+
+cd file_handling
+touch file_create.sh word_count.sh log_append.sh
+cd ..
+
+
+
+cd system 
+touch uptime.sh disk_usage.sh user.sh 
+cd ..
+
+
+
+cd projects
+touch calculator.sh prime_numbers.sh factorial.sh backup.sh
+cd ..
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
